f000e88a024cffbe9051e341dc7886a8b382cf45
Francisco Duque <fduque@saraswati.aei.mpg.de>
2025-01-31 10:54:38 +0100

    changed files for the pipeline

--- pipeline/fim_EMRI.py
+++ pipeline/fim_EMRI.py
@@ -1,6 +1,6 @@
 """
 Parameter estimation precision for EMRIs, averaged over nuisance parameters
-python fim_EMRI.py science_obj example_psd.npy 10.0 1.0 --use_gpu --device 7 --parameter_file EMRI_parameters.npy 
+python fim_EMRI.py science_obj example_psd.npy 10.0 --use_gpu --device 7 --parameter_file EMRI_parameters.npy 
 """
 import os
 import logging
@@ -23,13 +23,13 @@ logger = logging.getLogger()
 parser = argparse.ArgumentParser()
 
 # Add command line arguments
+parser.add_argument("parameter_file", help="Path to a file containing source parameters, organised as (N_source, N_montecarlo, N_params)")
 parser.add_argument("outdir", help="Where to save files")
-parser.add_argument("psd_file", help="Path to a file containing PSD frequency-value pairs")
-parser.add_argument("dt", help="Sampling cadence in seconds", type=float)
-parser.add_argument("T", help="Waveform duration in years.", type=float)
 
+parser.add_argument("--psd_file", help="Path to a file containing PSD frequency-value pairs", default = "TDI2_AE_psd.npy")
+parser.add_argument("--dt", help="Sampling cadence in seconds", type=float, default = 10)
+parser.add_argument("--Tobs", help="Waveform duration in years.", type=float)
 parser.add_argument("--use_gpu", help="Whether to use GPU for FIM computation", action="store_true")
-parser.add_argument("--parameter_file", help="Path to a file containing source parameters, organised as (N_source, N_montecarlo, N_params)")
 parser.add_argument("--deltas_file", help="Path to a file containing stable delta values for all sources, organised as (N_source, N_montecarlo, N_params)")
 parser.add_argument("--seed", help="numpy seed for random operations.",  action="store_const", const=42)
 parser.add_argument("--device", help="GPU device", type=int, default=0)
@@ -47,8 +47,31 @@ if args.use_gpu:
 
 np.random.seed(args.seed)
 
+inspiral_kwargs = {
+        "DENSE_STEPPING": 0,
+        "max_init_len": int(1e5),
+        "err": 1e-12,  
+        #"use_rk4": True,
+}
+
 # Generate EMRI waveform
-base_wave = GenerateEMRIWaveform("FastKerrEccentricEquatorialFlux", use_gpu=args.use_gpu, sum_kwargs=dict(pad_output=True))
+base_wave = GenerateEMRIWaveform("FastKerrEccentricEquatorialFlux", inspiral_kwargs = inspiral_kwargs,  use_gpu=args.use_gpu, sum_kwargs=dict(pad_output=True))
+
+#transformed wave
+class transf_log_e_wave():
+    def __init__(self, base_wave):
+        self.base_wave = base_wave
+
+    def __call__(self, *args, **kwargs):
+        args = list(args)
+        args[4] = np.exp(args[4]) #index of eccentricity on the FEW waveform call
+        return self.base_wave(*args, **kwargs)
+    
+    def __getattr__(self, name):
+        # Forward attribute access to base_wave
+        return getattr(self.base_wave, name)
+
+transf_log_e_wave = transf_log_e_wave(base_wave)
 
 # Order of the langrangian interpolation
 order = 25
@@ -67,56 +90,13 @@ tdi_kwargs_esa = dict(
     orbit_kwargs=orbit_kwargs_esa, order=order, tdi=tdi_gen, tdi_chan="AET",
 )
 
-# Initialize ResponseWrapper model
-model = ResponseWrapper(
-    base_wave,
-    args.T,
-    args.dt,
-    index_lambda,
-    index_beta,
-    t0=100000.,
-    flip_hx=True,  # Set to True if waveform is h+ - ihx
-    use_gpu=args.use_gpu,
-    remove_sky_coords=False,  # True if the waveform generator does not take sky coordinates
-    is_ecliptic_latitude=False,  # False if using polar angle (theta)
-    remove_garbage=True,  # Removes the beginning of the signal that has bad information
-    **tdi_kwargs_esa,
-)
+#Load detector frame parameter file organized as (N_source, N_montecarlo, N_params), 
+# where N_params are organized as [M, mu, a, p0, e0, Y0, dist, qS, phiS, thetaS, qK, phiK, Phi_phi0, Phi_theta0, Phi_r0, T_coalescence, SNR to coalescence]
+params_file = np.load(args.parameter_file)
 
-# test model
-# model(1e6, 10.0, 0.0, 10.0, 0.3, 1.0, 1.0, np.pi/3, np.pi/3, np.pi/3, np.pi/3, np.pi/3, np.pi/3, np.pi/3)
-
-traj_model = base_wave.waveform_generator.inspiral_generator
-
-# Generate or load parameter file
-if args.parameter_file is None:
-    logger.critical("--parameter_file not supplied. This file will now be produced.")
-    
-    Ms = [5e5, 1e6, 5e6] 
-    mu = [1e1, 1e1, 1e1] 
-    ecc = [0.1, 0.1, 0.1] 
-    spin = [0.9, 0.9, 0.9] 
-    xI = [1., 1., 1.]
-
-    redshift = [1.0, 1.0, 1.0]
-    dists = [standard_cosmology(H0=67.).dl_zH0(el) / 1000. for el in redshift]
-
-    p0 = []
-    print("getting p0 values")
-    for i in range(len(Ms)):
-        p0v = get_p_at_t(traj_model, args.T*1.001, [Ms[i], mu[i], spin[i], ecc[i], xI[i]], 
-                         bounds=[get_separatrix(spin[i], ecc[i], xI[i]) + 0.5, 16.]) # Bounds are very important here for working, ultimately we will just store the parameters
-        p0.append(p0v)
-
-    fixed_pars = np.vstack((Ms, mu, spin, p0, ecc, xI, dists)).T
-    params_file = draw_sources(fixed_pars, N_per_source=10, seed=args.seed)
-
-    np.save(args.outdir + "/EMRI_parameters", params_file)
-    # print shape of the data array
-    print(params_file.shape)
-
-else:
-    params_file = np.load(args.parameter_file)
+#If Tobs is set by the user, overwrite the Tobs in the parameter file
+if args.Tobs is not None:
+    params_file[:, :, -2] = args.T 
 
 # Generate or load deltas file
 if args.deltas_file is None:
@@ -132,22 +112,38 @@ psdf, psdv = np.load(args.psd_file).T
 psd_interp = CubicSplineInterpolant(psdf, psdv, use_gpu=args.use_gpu)
 psd_wrap = lambda f, **kwargs: psd_interp(f)
 
+# Initialize ResponseWrapper model and run a test covariance matrix to check everything is working and determine shape
+model = ResponseWrapper(
+        transf_log_e_wave,
+        params_file[0, 0, -2],
+        args.dt,
+        index_lambda,
+        index_beta,
+        t0=100000.,
+        flip_hx=True,  # Set to True if waveform is h+ - ihx
+        use_gpu=args.use_gpu,
+        remove_sky_coords=False,  # True if the waveform generator does not take sky coordinates
+        is_ecliptic_latitude=False,  # False if using polar angle (theta)
+        remove_garbage=True,  # Removes the beginning of the signal that has bad information
+        **tdi_kwargs_esa,
+    )
+
 # Run a test covariance matrix to check everything is working and determine shape
-cov, fisher_object = get_covariance_matrix(
+cov, fisher_object, SNR = get_covariance_matrix(
     model, 
-    parameters=params_file[0,0], 
+    parameters=params_file[0,0,:-3], # Remove z, Tobs and SNR
     dt=args.dt, 
-    T=args.T, 
+    T=params_file[0,0,-2], 
     psd=psd_wrap, 
     use_gpu=args.use_gpu,
     outdir=args.outdir, 
+    log_e = True,
     deltas=deltas_file[0,0] if deltas_file is not None else None
 )
-# print("Covariance is")
-# print(cov)
 
-# print("Deltas:")
-# print(fisher_object.deltas)
+print("Precision is")
+print(np.sqrt(np.diag(cov)))
+
 
 Nsources = params_file.shape[0]
 N_montecarlo = params_file.shape[1]
@@ -160,6 +156,24 @@ deltas_cache = np.zeros((Nsources, N_montecarlo, cov.shape[0]))
 print(f"(Source num, random draw num) = ({Nsources, N_montecarlo})")
 for source_num in range(Nsources):
     print("------------------------------------------------------------")
+    
+    # Initialize ResponseWrapper model for different sources (can have different T)
+    model = ResponseWrapper(
+        transf_log_e_wave,
+        params_file[source_num, 0, -2], # Tobs
+        args.dt,
+        index_lambda,
+        index_beta,
+        t0=100000.,
+        flip_hx=True,  # Set to True if waveform is h+ - ihx
+        use_gpu=args.use_gpu,
+        remove_sky_coords=False,  # True if the waveform generator does not take sky coordinates
+        is_ecliptic_latitude=False,  # False if using polar angle (theta)
+        remove_garbage=True,  # Removes the beginning of the signal that has bad information
+        **tdi_kwargs_esa,
+    )
+
+    
     for montecarlo_num in range(N_montecarlo):
         print("^^^^^^^^^^^^^^^^^^")
         print(f"working on (Source num, random draw num) = ({source_num, montecarlo_num})")
@@ -170,16 +184,19 @@ for source_num in range(Nsources):
         
         # current folder
         current = '/source_{0}_draw_{1}'.format(source_num, montecarlo_num)
-        cov_here, fisher_object = get_covariance_matrix(
+        cov_here, fisher_object, SNR_here = get_covariance_matrix(
             model,
-            parameters=params_file[source_num, montecarlo_num],
+            parameters=params_file[source_num, montecarlo_num, :-3], # Remove z, Tobs and SNR
             dt=args.dt,
-            T=args.T,
+            T=params_file[source_num, montecarlo_num, -2],
             psd=psd_wrap,
             use_gpu=args.use_gpu,
             outdir=args.outdir + current,
+            log_e = True,
             deltas=deltas,
         )
+
+        params_file[source_num, montecarlo_num, -1] = SNR_here # Update SNR
 
         # save fisher deltas
         fisher_object.save_deltas()
@@ -188,9 +205,9 @@ for source_num in range(Nsources):
         # save params
         np.save(args.outdir + current + f"/source_{source_num}_draw_{montecarlo_num}_params", params_file[source_num, montecarlo_num])
         # print relative precision
-        print("Relative precision is")
+        print("Precision is")
         print(fisher_object.param_names)
-        print(np.sqrt(np.diag(cov_here)) / np.delete(params_file[source_num, montecarlo_num], [5, 12]))
+        print(np.sqrt(np.diag(cov_here))) #/ np.delete(params_file[source_num, montecarlo_num], [5, 12, 13, 14]))
         # save plot
         CovEllipsePlot(fisher_object.param_names, fisher_object.wave_params, cov_here, filename=args.outdir + current + f"/covariance_ellipses.png")
         cov_out[source_num, montecarlo_num] = cov_here
---
+++ pipeline/generate_source_backwards.py
@@ -0,0 +1,278 @@
+import os
+import logging
+import argparse
+import numpy as np
+from common import standard_cosmology
+from few.utils.constants import *
+from few.trajectory.inspiral import EMRIInspiral
+from few.waveform import (
+    GenerateEMRIWaveform,
+)
+from few.utils.constants import *
+
+from few.utils.utility import (
+    get_separatrix,
+)
+from few.trajectory.ode import *
+from scipy.interpolate import interp1d
+from fastlisaresponse import ResponseWrapper
+from scipy.interpolate import CubicSpline
+from few.summation.interpolatedmodesum import CubicSplineInterpolant
+from stableemrifisher.fisher import StableEMRIFisher
+import pandas as pd
+
+
+logger = logging.getLogger()
+parser = argparse.ArgumentParser()
+
+# Add command line arguments
+parser.add_argument("parameter_file_source", help="Path to a npy file containing source frame parameters at plunge organised as (N_source, N_params_source)")
+#N_params_source are organised as (M, mu, a, e_f, i_f, z, T to integrate backwards)
+parser.add_argument("parameter_file_detector", help="Path to a npy file to save detector frame parameters at start of observation")
+
+parser.add_argument("--psd_file", help="Path to a file containing PSD frequency-value pairs", default="TDI2_AE_psd.npy")
+parser.add_argument("--dt", help="Sampling cadence in seconds", type=float, default=10.0)
+parser.add_argument("--use_gpu", help="Whether to use GPU for FIM computation", action="store_true")
+parser.add_argument("--N_montecarlo", help="How many random sky localizations to generate", type=int, default=10)
+parser.add_argument("--seed", help="numpy seed for random operations.",  action="store_const", const=42)
+parser.add_argument("--device", help="GPU device", type=int, default=7)
+
+args = parser.parse_args()
+
+if args.use_gpu:
+    import cupy as xp
+    print("Using GPU", args.device)
+    xp.cuda.Device(args.device).use()
+    xp.random.seed(args.seed)
+else:
+    xp = np
+
+
+np.random.seed(args.seed)
+
+# Set type of inspiral trajectory
+trajectory_class = KerrEccEqFlux
+
+traj = EMRIInspiral(func=trajectory_class)
+
+inspiral_kwargs_back = {
+    "DENSE_STEPPING": 0,  # Set to 1 if we want a sparsely sampled trajectory
+    "max_init_len": int(1e5),  # All of the trajectories will be well under len = 10^5
+    "err": 1e-12,  # Set error tolerance on integrator -- RK8
+    "integrate_backwards": True,  # Integrate trajectories backwards
+}
+
+
+inspiral_kwargs_forward = {
+    "DENSE_STEPPING": 0,  # Set to 1 if we want a sparsely sampled trajectory
+    "max_init_len": int(1e5),  # all of the trajectories will be well under len = 10^5
+    "err": 1e-12,  # Set error tolerance on integrator -- RK8
+    "integrate_backwards": False,  # Integrate trajectories forwards
+}
+
+# Generate EMRI waveform
+base_wave = GenerateEMRIWaveform("FastKerrEccentricEquatorialFlux", inspiral_kwargs = inspiral_kwargs_forward,  use_gpu=args.use_gpu, sum_kwargs=dict(pad_output=True))
+
+# Order of the langrangian interpolation
+order = 25
+
+# Orbit file and kwargs
+orbit_file_esa = "../lisa-on-gpu/orbit_files/esa-trailing-orbits.h5"
+orbit_kwargs_esa = dict(orbit_file=orbit_file_esa)
+
+# TDI generation
+tdi_gen = "2nd generation"
+index_lambda = 8
+index_beta = 7
+
+# TDI kwargs
+tdi_kwargs_esa = dict(
+    orbit_kwargs=orbit_kwargs_esa, order=order, tdi=tdi_gen, tdi_chan="AET",
+)
+
+# PSD 
+psd_file = "TDI2_AE_psd.npy"
+psdf, psdv = np.load(psd_file).T
+psd_interp = CubicSplineInterpolant(psdf, psdv,  use_gpu=args.use_gpu)
+psd_wrap = lambda f, **kwargs: psd_interp(f)
+
+source_args_plunge = np.load(args.parameter_file_source)
+source_args_initial = np.zeros((len(source_args_plunge), args.N_montecarlo, 17)) #16 = 14 parameters in vacuum + z + Tobs + SNR
+
+z = source_args_plunge[:, 5]
+
+M = source_args_plunge[:, 0] * (1 + z) #detector frame conversion
+mu = source_args_plunge[:, 1] * (1 + z) #detector frame conversion
+a = source_args_plunge[:, 2]
+e_f = source_args_plunge[:, 3]
+Y_f = np.cos(source_args_plunge[:, 4])
+p_f = np.array([])
+dist = np.array([])
+for i in range(len(source_args_plunge)):
+    p_f = np.append(p_f, get_separatrix(a[i], e_f[i], Y_f[i])) + 0.1 # add a small number to separatrix to integrate backwards
+    dist = np.append(dist, standard_cosmology(H0=67.).dl_zH0(z[i]) / 1000.)
+
+T = source_args_plunge[:, 6]
+
+#Parameters to be varied in the Fisher Matrix for Kerr Eccentric model (problem when e0 = 0)
+param_names = [
+    'M',
+    'mu',
+    'a',
+    'p0',
+    'e0',
+    'qS',
+    'phiS',
+    'qK',
+    'phiK',
+    'Phi_phi0',
+    'Phi_r0',
+]
+
+for i in np.arange(len(source_args_plunge)):
+    
+    # Create a DataFrame for the source parameters
+    source_frame_data = {
+        "Source": [i],
+        "M": [M[i] / (1 + z[i])],
+        "μ": [mu[i] / (1 + z[i])],
+        "a": [a[i]],
+        "p_f": [p_f[i]],
+        "e_f": [e_f[i]],
+        "i_f": [source_args_plunge[i, 4]],
+        "z": [z[i]],
+    }
+
+    #Convert the data dictionary into a DataFrame
+    df = pd.DataFrame(source_frame_data)
+
+    # Print the DataFrame in a readable format
+    print("Source Frame Parameters at Plunge:\n")
+    print(df.to_string(index=False, float_format="%.2e"))
+    print("\n")
+
+    # Generate random initial phases for the trajectory
+    Phi_phi_f = np.random.uniform(0, 2 * np.pi, args.N_montecarlo)
+    Phi_theta_f = np.random.uniform(0, 2 * np.pi, args.N_montecarlo)
+    Phi_r_f = np.random.uniform(0, 2 * np.pi, args.N_montecarlo)
+    
+    # Initialize ResponseWrapper model
+    model = ResponseWrapper(
+        base_wave,
+        T[i],
+        args.dt,
+        index_lambda,
+        index_beta,
+        t0=100000.,
+        flip_hx=True,  # Set to True if waveform is h+ - ihx
+        use_gpu=args.use_gpu,
+        remove_sky_coords=False,  # True if the waveform generator does not take sky coordinates
+        is_ecliptic_latitude=False,  # False if using polar angle (theta)
+        remove_garbage=True,  # Removes the beginning of the signal that has bad information
+        **tdi_kwargs_esa,
+    )
+
+    # Loop over sky localizations/initial phases
+    for j in np.arange(args.N_montecarlo):
+        # Integrate trajectory backwards
+        t_back, p_back, e_back, Y_back, Phi_phi_back, Phi_r_back, Phi_theta_back = (
+        traj(
+            M[i],
+            mu[i],
+            a[i],
+            p_f[i],
+            e_f[i], 
+            Y_f[i],
+            Phi_phi0=Phi_phi_f[j],
+            Phi_theta0=Phi_theta_f[j],
+            Phi_r0=Phi_r_f[j],
+            dt=args.dt,
+            T=T[i],
+            **inspiral_kwargs_back
+        )
+        )
+
+        p0 = p_back[-1]
+        e0 = e_back[-1]
+        Y0 = Y_back[-1]
+        Phi_phi0 = Phi_phi_back[-1]
+        Phi_r0 = Phi_r_back[-1]
+        Phi_theta0 = Phi_theta_back[-1]
+
+        # SNR threshold for the Fisher Matrix calculation
+        SNR_threshold = 25.0  
+
+        # Define the maximum number of iterations for sky localization generation to avoid infinite loops 
+        max_iterations = 100
+
+        iterations = 0
+        SNR = 0
+
+        while SNR < SNR_threshold and iterations < max_iterations:
+        # Generate random sky localization parameters
+            qS = np.pi/2 - np.arcsin(np.random.uniform(-1, 1))
+            phiS = np.random.uniform(0, 2 * np.pi)
+            qK = np.pi/2 - np.arcsin(np.random.uniform(-1, 1))
+            phiK = np.random.uniform(0, 2 * np.pi)
+
+            # Create parameter list
+            parameters = [
+                M[i], mu[i], a[i], p0, e0, Y0, dist[i], qS, phiS, qK, phiK, 
+                Phi_phi0, Phi_theta0, Phi_r0
+            ]
+    
+            # Create StableEMRIFisher object and calculate SNR
+            sef = StableEMRIFisher(*parameters, dt=args.dt, T=T[i], EMRI_waveform_gen=model, noise_model=psd_wrap, 
+                              noise_kwargs=dict(TDI="TDI2"), param_names=param_names, stats_for_nerds=False, 
+                              use_gpu=args.use_gpu, der_order=4)
+            SNR = sef.SNRcalc_SEF()  # Calculate the SNR
+            print(f"Iteration {iterations + 1}: SNR = {SNR}")
+    
+            iterations += 1
+
+        if SNR >= SNR_threshold:
+            #print(f"Success: SNR = {SNR} exceeded the threshold after {iterations} iterations.")
+            source_args_initial[i, j, : -3] = np.array(parameters)
+            # Store Tobs and SNR in the last two columns 
+            source_args_initial[i, j, -3] = z[i]
+            source_args_initial[i, j, -2] = T[i] 
+            source_args_initial[i, j, -1] = SNR
+        else:
+            print(f"Failed to achieve SNR above threshold after {max_iterations} iterations.\n")
+
+# Convert source_args_initial to a DataFrame for better readability in the print
+columns = [
+    "M", "mu", "a", "p0", "e0", "Y0", "dist", "qS", "phiS", 
+    "qK", "phiK", "Phi_phi0", "Phi_theta0", "Phi_r0", "z", "T_coalescence", "SNR"
+] #Add Tobs and SNR at the end of the columns
+
+# Reshape the 3D array into 2D, keeping track of (Nsource, Nmontecarlo) indices
+reshaped_data = source_args_initial.reshape(-1, source_args_initial.shape[-1])
+
+# Generate indices for source type (Nsource) and individual source (Nmontecarlo)
+num_sources_per_type = source_args_initial.shape[1]
+source_types = np.repeat(np.arange(source_args_initial.shape[0]), num_sources_per_type)
+individual_sources = np.tile(np.arange(num_sources_per_type), source_args_initial.shape[0])
+
+# Create the DataFrame and add the indices for clarity
+df = pd.DataFrame(reshaped_data, columns=columns)
+df.insert(0, "Source Type", source_types)
+df.insert(1, "Source Index", individual_sources)
+
+# Compute average SNR for all sources
+average_snr = df["SNR"].mean()
+
+# Compute average SNR per source type
+average_snr_per_type = df.groupby("Source Type")["SNR"].mean()
+
+# Print the DataFrame
+print("\n Detector frame parameters at start of observation:\n")
+print(df.to_string(index=False, float_format="%.2e"))
+
+
+# Print average SNR per source type
+print("\nAverage SNR per Source Type:")
+print(average_snr_per_type.to_string(float_format="%.2e"))
+
+# Save the data
+np.save(args.parameter_file_detector, source_args_initial)
